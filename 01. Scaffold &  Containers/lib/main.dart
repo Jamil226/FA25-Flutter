ac47a0399bc79fd02819f74a9f8986e80cc86ba0
Jamil226 <jamil138.amin@gmail.com>
2025-09-04 14:22:13 +0500

    updated-code

--- 01. Scaffold &  Containers/lib/main.dart
+++ 01. Scaffold &  Containers/lib/main.dart
@@ -6,56 +6,126 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.teal,
         body: SafeArea(
-          //     child: Container(
-          //   height: 200.0,
-          //   width: 200.0,
-          //   margin: EdgeInsets.fromLTRB(10.0, 20.0, 10.0, 10.0),
-          //   padding: EdgeInsets.all(20.0),
-          //   color: Colors.white,
-          //   child: Text("Container 1"),
+          // child: Container(
+          //   height: 100.0,
+          //   width: 450.0,
+          //   color: Colors.green,
+          //   // padding: EdgeInsets.all(10.0),
+          //   padding: EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 5.0),
+          //   // margin: EdgeInsets.all(30.0),
+          //   margin: EdgeInsets.fromLTRB(30.0, 10.0, 30.0, 40.0),
+          //   child: Text('Hello World'),
           // )
-          child: Row(
-            // crossAxisAlignment: CrossAxisAlignment.stretch,
+          // child: Row(
+          //   children: [
+          //   Container(
+          //     height: 100.0,
+          //     width: 100.0,
+          //     color: Colors.green,
+          //     // padding: EdgeInsets.all(10.0),
+          //     padding: EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 5.0),
+          //     // margin: EdgeInsets.all(30.0),
+          //     margin: EdgeInsets.fromLTRB(30.0, 10.0, 30.0, 40.0),
+          //     child: Text('Hello World'),
+          //   ),
+          //     Container(
+          //       height: 100.0,
+          //       width: 100.0,
+          //       color: Colors.green,
+          //       // padding: EdgeInsets.all(10.0),
+          //       padding: EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 5.0),
+          //       // margin: EdgeInsets.all(30.0),
+          //       margin: EdgeInsets.fromLTRB(30.0, 10.0, 30.0, 40.0),
+          //       child: Text('Hello World'),
+          //     ),
+          //     Container(
+          //       height: 100.0,
+          //       width: 100.0,
+          //       color: Colors.green,
+          //       // padding: EdgeInsets.all(10.0),
+          //       padding: EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 5.0),
+          //       // margin: EdgeInsets.all(30.0),
+          //       margin: EdgeInsets.fromLTRB(30.0, 10.0, 30.0, 40.0),
+          //       child: Text('Hello World'),
+          //     )
+          //   ],
+          // ),
+
+          //   child: Column(
+          //     children: [
+          //     Container(
+          //       height: 300.0,
+          //       width: 300.0,
+          //       color: Colors.green,
+          //       // padding: EdgeInsets.all(10.0),
+          //       padding: EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 5.0),
+          //       // margin: EdgeInsets.all(30.0),
+          //       margin: EdgeInsets.fromLTRB(30.0, 10.0, 30.0, 40.0),
+          //       child: Text('Hello World'),
+          //     ),
+          //       Container(
+          //         height: 300.0,
+          //         width: 300.0,
+          //         color: Colors.green,
+          //         // padding: EdgeInsets.all(10.0),
+          //         padding: EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 5.0),
+          //         // margin: EdgeInsets.all(30.0),
+          //         margin: EdgeInsets.fromLTRB(30.0, 10.0, 30.0, 40.0),
+          //         child: Text('Hello World'),
+          //       ),
+          //       Container(
+          //         height: 300.0,
+          //         width: 300.0,
+          //         color: Colors.green,
+          //         // padding: EdgeInsets.all(10.0),
+          //         padding: EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 5.0),
+          //         // margin: EdgeInsets.all(30.0),
+          //         margin: EdgeInsets.fromLTRB(30.0, 10.0, 30.0, 40.0),
+          //         child: Text('Hello World'),
+          //       )
+          //     ],
+          //
+          //
+          // ),
+          child: ListView(
             children: [
               Container(
-                height: 200.0,
-                width: 100.0,
-                margin: EdgeInsets.all(20.0),
-                padding: EdgeInsets.all(20.0),
-                color: Colors.white,
-                child: Text("Container 1"),
-              ),
-              SizedBox(
-                width: 10.0,
-              ),
-              Container(
-                height: 200.0,
-                width: 100.0,
-                color: Colors.red,
-                padding: EdgeInsets.all(20.0),
-                child: Text("Container 2"),
-              ),
-              SizedBox(
-                width: 20.0,
+                height: 300.0,
+                width: 300.0,
+                color: Colors.green,
+                // padding: EdgeInsets.all(10.0),
+                padding: EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 5.0),
+                // margin: EdgeInsets.all(30.0),
+                margin: EdgeInsets.fromLTRB(30.0, 10.0, 30.0, 40.0),
+                child: Text('Hello World'),
               ),
               Container(
-                // height: double.infinity,
-                height: 200.0,
-                width: 100.0,
-                color: Colors.blue,
-                padding: EdgeInsets.all(20.0),
-                child: Text("Container 2"),
+                height: 300.0,
+                width: 300.0,
+                color: Colors.green,
+                // padding: EdgeInsets.all(10.0),
+                padding: EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 5.0),
+                // margin: EdgeInsets.all(30.0),
+                margin: EdgeInsets.fromLTRB(30.0, 10.0, 30.0, 40.0),
+                child: Text('Hello World'),
               ),
-              // Container(
-              //   width: double.infinity,
-              // ),
+              Container(
+                height: 300.0,
+                width: 300.0,
+                color: Colors.green,
+                // padding: EdgeInsets.all(10.0),
+                padding: EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 5.0),
+                // margin: EdgeInsets.all(30.0),
+                margin: EdgeInsets.fromLTRB(30.0, 10.0, 30.0, 40.0),
+                child: Text('Hello World'),
+              ),
             ],
           ),
         ),
